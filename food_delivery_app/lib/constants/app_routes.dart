58d6370a803e1b1a044afd483e044bc37a543056
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2021-04-13 13:00:16 +0200

    Project initial setup

---
+++ food_delivery_app/lib/constants/app_routes.dart
@@ -0,0 +1,9 @@
+import 'package:food_delivery_app/presentation/presentation.dart';
+
+class AppRoutes {
+  static String home = '/home_page';
+
+  static var routes = {
+    home: (_) => HomePage(),
+  };
+}
---
+++ food_delivery_app/lib/presentation/widgets/widgets.dart
@@ -0,0 +1,2 @@
+export 'app_text_display.dart';
+export 'app.dart';
