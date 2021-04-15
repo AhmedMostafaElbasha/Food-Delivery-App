75b7fa3a14777f4cec105efe3c52682349406b5a
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2021-04-15 17:39:48 +0200

    Added Images to Project

--- food_delivery_app/lib/constants/app_image_paths.dart
+++ food_delivery_app/lib/constants/app_image_paths.dart
@@ -1,1 +1,14 @@
-class AppImagePaths {}
+class AppImagePaths {
+  static String beer = 'assets/beer.png';
+  static String breakfastJpeg = 'assets/breakfast.jpeg';
+  static String breakfast = 'assets/breakfast.png';
+  static String burger = 'assets/burger.png';
+  static String cheeseBurger = 'assets/cheeseburger.png';
+  static String coffeeCup = 'assets/coffee-cup.png';
+  static String cupCake = 'assets/cupcake.png';
+  static String lunch = 'assets/lunch.jpeg';
+  static String noImage = 'assets/noimage.png';
+  static String pizza = 'assets/pizza.png';
+  static String supper1 = 'assets/supper_1.jpeg';
+  static String turkey = 'assets/turkey.png';
+}
